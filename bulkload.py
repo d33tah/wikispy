e35d75038325dd3e6a9bf6ef9dfe2d2d65ed8a07
Jacek Wielemborek <d33tah@gmail.com>
2015-04-10 16:15:55 +0200

    Add more fields to bulk loading.

--- bulkload.py
+++ bulkload.py
@@ -3,17 +3,24 @@
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wikispy.settings")
 
-from wikispy.models import Edit
+from wikispy.models import Edit, Wiki
 import ast
 import sys
 from django.db import transaction
 
 def main():
     with transaction.atomic():
+        w = Wiki()
+        w.name = "plwiki"
+        w.save()
         for line in sys.stdin:
             fields = ast.literal_eval(line)
             e = Edit()
+            e.ip = fields[0]
+            e.title = fields[1]
+            #e.time = fields[2]
             e.wikipedia_id = int(fields[-1].split('=')[-1])
+            e.wiki = w
             e.save()
 
 if __name__ == "__main__":
--- wikispy/models.py
+++ wikispy/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 class Edit(models.Model):
     wikipedia_id = models.IntegerField()
+    title = models.CharField(max_length=1024)
+    ip = models.GenericIPAddressField()
+    wiki = models.ForeignKey('Wiki')
+
+class Wiki(models.Model):
+    name = models.CharField(max_length=1024)
