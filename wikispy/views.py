d7dccc8a17ee7caa95be669c6cc04022520a2782
Jacek Wielemborek <d33tah@gmail.com>
2015-04-10 16:47:41 +0200

    For now, just display a table of 10 edits in the database.

--- wikispy/views.py
+++ wikispy/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from wikispy.models import Edit
 
 def index(request):
-    edits = Edit.objects.filter(wikipedia_id='16702065')
+    edits = Edit.objects.all()[:10]
     return render(request, 'index.html', {'edits': edits})
