795909d1a7e39f49d0205807b6850bda679ad7b1
Jacek Wielemborek <d33tah@gmail.com>
2015-04-18 15:44:47 +0200

    Make it possible to translate errors and give them a separate view, detect when there are no rows to be displayed.

--- wikispy/views.py
+++ wikispy/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from wikispy.models import get_edits_by_rdns
 from django.http import HttpResponseRedirect, HttpResponse
-
+from django.utils.translation import ugettext as _
+import itertools
 
 def index(request):
     return HttpResponseRedirect('/by_rdns/plwiki/.gov.pl')
@@ -11,13 +12,19 @@ def by_rdns(request, wiki_name, rdns):
     if not rdns.startswith('.'):
         rdns = '.' + rdns
     if '%' in rdns:
-        return error(request, "rDNS cannot contain % sign.")
+        return error(request, _("rDNS cannot contain % sign."))
     edits = get_edits_by_rdns(rdns, wiki_name)
-    return render(request, 'index.html', {'edits': edits})
+    # Let's see if it has any items...
+    try:
+        first = next(edits)
+    except StopIteration:
+        return error(request, _("No results were found."))
+    return render(request, 'index.html',
+                  {'edits': itertools.chain([first], edits)})
 
 
 def error(request, error_str):
-    return HttpResponse(error_str)
+    return render(request, 'error.html', {'error': error_str})
 
 
 def rules(request):
