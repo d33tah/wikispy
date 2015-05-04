d33tah/wikispy

commit cce220b556caca7130acf4856946dc083a989ed8

Add the "next page" trick I mentioned in Github issue #3.

wikispy/views.py
@@ -33,12 +33,28 @@ def peek_results(edits, pagesize):
        iterating over it (the original might have some elements removed) and
        a boolean value telling whether we should display a link to the new
        page."""
+    taken = []
     try:
-        first = [next(edits)]
+        taken += [next(edits)]
     except StopIteration:
         return None, False
+    if pagesize != 0:
+        has_next_page = True
+        i = 0
+        while True:
+            try:
+                taken += [next(edits)]
+            except StopIteration:
+                has_next_page = False
+                break
+            i += 1
+            if i >= pagesize:
+                break
+    else:
+        has_next_page = False
+    iterator = itertools.islice(itertools.chain(taken, edits), 0, pagesize)
+    return iterator, has_next_page
 
-    return itertools.chain(first, edits), True
 
 def validate_pagesize_and_offset(f):
     """A function decorator that sanitizes the pagesize and offset view
@@ -73,7 +89,7 @@ def validate_rdns(f):
 def by_rdns(request, wiki_name, rdns, offset, pagesize):
 
     try:
-        edits = get_edits(wiki_name, offset, pagesize, rdns=rdns)
+        edits = get_edits(wiki_name, offset, pagesize + 1, rdns=rdns)
     except ValueError:
         return error(request, _("The query is too big."))
 
@@ -96,7 +112,7 @@ def by_rdns(request, wiki_name, rdns, offset, pagesize):
 @validate_pagesize_and_offset
 def by_ip(request, wiki_name, startip, endip, offset, pagesize):
 
-    edits = get_edits(wiki_name, offset, pagesize, startip=startip,
+    edits = get_edits(wiki_name, offset, pagesize + 1, startip=startip,
                       endip=endip)
 
     edits_checked, has_next_page = peek_results(edits, pagesize)

commit b82a31930885f402fea484cd6dd65d8e49b06d01

Fix a bug caused in commit cce220b5 (related to Github Issue #3).

wikispy/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from django.db import connection
 import json
+import itertools
 from django.utils.translation import ugettext as _
 
 
@@ -126,8 +127,8 @@ def query_scans_table(query, table):
     sql, params = query.sql_with_params()
     return sql_scans_table(sql, table, params)
 
-def get_edits(wikiname, offset=0, limit=50, random=False, rdns=None,
-              startip=None, endip=None, wikipedia_edit_id=None):
+def get_edits_internal(wikiname, offset=0, limit=50, random=False, rdns=None,
+                       startip=None, endip=None, wikipedia_edit_id=None):
     cursor = connection.cursor()
     cursor.execute("SET enable_seqscan=off;")
 
@@ -137,7 +138,7 @@ def get_edits(wikiname, offset=0, limit=50, random=False, rdns=None,
     offset_sql = ""
     if not random:
         if limit != 0:
-            limit_sql = "\nLIMIT %s" % int(limit)
+            limit_sql = "\nLIMIT %s" % int(limit + 1)
         offset_sql = "\nOFFSET %s" % int(offset)
     else:
         order_sql = '\nORDER BY random()'
@@ -182,6 +183,16 @@ def get_edits(wikiname, offset=0, limit=50, random=False, rdns=None,
     for row in cursor:
         yielded = dict(zip(description, row))
         yield yielded
+
+def get_edits(wikiname, offset=0, limit=50, random=False, rdns=None,
+              startip=None, endip=None, wikipedia_edit_id=None):
+    ret = get_edits_internal(wikiname, offset, limit, random,
+                             rdns, startip, endip, wikipedia_edit_id)
+    if limit is not None and limit != 0:
+        return itertools.islice(ret, 0)
+    else:
+        return ret
+
 
 def mark_watched(ip, wiki, wikipedia_edit_id):
     vq = ViewRecord.objects.filter(ip=ip, wiki__id=wiki.id,

wikispy/views.py
@@ -62,8 +62,7 @@ def peek_results(edits, pagesize):
                 break
     else:
         has_next_page = False
-    iterator = itertools.islice(itertools.chain(taken, edits), 0, pagesize)
-    return iterator, has_next_page
+    return itertools.chain(taken, edits), has_next_page
 
 
 def validate_pagesize_and_offset(f):
@@ -99,7 +98,7 @@ def validate_rdns(f):
 def by_rdns(request, wiki_name, rdns, offset, pagesize):
 
     try:
-        edits = get_edits(wiki_name, offset, pagesize + 1, rdns=rdns)
+        edits = get_edits(wiki_name, offset, pagesize, rdns=rdns)
     except ValueError:
         return error(request, _("The query is too big."))
 
@@ -122,7 +121,7 @@ def by_rdns(request, wiki_name, rdns, offset, pagesize):
 @validate_pagesize_and_offset
 def by_ip(request, wiki_name, startip, endip, offset, pagesize):
 
-    edits = get_edits(wiki_name, offset, pagesize + 1, startip=startip,
+    edits = get_edits(wiki_name, offset, pagesize, startip=startip,
                       endip=endip)
 
     edits_checked, has_next_page = peek_results(edits, pagesize)